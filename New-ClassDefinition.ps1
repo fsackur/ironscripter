009effad97dd4e11d2dcc664d8e6b1ae351c72e3
Freddie Sackur <Freddie.Sackur@rackspace.co.uk>
2021-01-06 21:36:02 +0000

    Methods added

--- New-ClassDefinition.ps1
+++ New-ClassDefinition.ps1
@@ -24,7 +24,7 @@ function New-ClassDefinition
 
 
     [void]$Def.AppendLine("#region Properties")
-    $Properties = $InputObject.PSObject.Properties
+    $Properties = $InputObject.PSObject.Properties | Sort-Object IsInstance, Name
     foreach ($Property in $Properties)
     {
         $Type = $Property.TypeNameOfValue -replace '^System.'
@@ -37,6 +37,46 @@ function New-ClassDefinition
         [void]$Def.Append("$").AppendLine($Property.Name)
     }
     [void]$Def.AppendLine("#endregion Properties")
+
+
+
+    [void]$Def.AppendLine().AppendLine()
+
+
+
+    [void]$Def.AppendLine("#region Methods")
+    $Methods = $InputObject.PSObject.Methods | Sort-Object IsInstance, Name
+    foreach ($Method in $Methods)
+    {
+        $Type = $Method.Value -replace ' .*' -replace '^System.'
+        $Params = $Method.Value -replace '.*\(' -replace '\)' -split ', '
+        [void]$Def.Append("[").Append($Type).Append("] ")
+        [void]$Def.Append($Method.Name)
+
+        [void]$Def.Append("(")
+        if (-not [string]::IsNullOrWhiteSpace($Params))
+        {
+            foreach ($Param in $Params)
+            {
+                $PType, $PName = $Param -split ' '
+                $PType = $PType -replace '^System\.'
+                [void]$Def.Append("[").Append($PType).Append("]")
+                [void]$Def.Append("$").Append($PName)
+                [void]$Def.Append(", ")
+            }
+            # Clear the redundant trailing comma
+            $Def.Length -= 2
+        }
+        [void]$Def.AppendLine(")")
+
+        [void]$Def.AppendLine("{")
+        [void]$Def.AppendLine("# Replace with your own method definition")
+        [void]$Def.Append("return [").Append($Type).AppendLine("]::new()")
+        [void]$Def.AppendLine("}")
+    }
+    [void]$Def.AppendLine("#endregion Methods")
+
+
 
     [void]$Def.AppendLine("}")
 
